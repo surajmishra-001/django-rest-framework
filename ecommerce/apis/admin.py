978f7c944a06df952379cbb34b2079f5702bb524
Suraj mishra <140611249+surajmishra-001@users.noreply.github.com>
2023-09-20 14:41:58 +0530

    product model created

--- ecommerce/apis/admin.py
+++ ecommerce/apis/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Attribute)
+admin.site.register(Brand)
+class InHouseProductsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'product_type', 'unit_price', 'total_quantity')  # Customize the displayed columns in the list view
+
+    fieldsets = (
+        ('General Information', {
+            'fields': ('name', 'description', 'product_type', 'category', 'sku', 'unit', 'color', 'unit_price', 'purchase_price')
+        }),
+        ('Pricing and Discounts', {
+            'fields': ('tax', 'discount', 'discount_type', 'shipping_cost')
+        }),
+        ('Inventory', {
+            'fields': ('total_quantity', 'minimum_order_quantity')
+        }),
+        ('SEO', {
+            'fields': ('meta_title', 'meta_description', 'meta_image')
+        }),
+        ('Images', {
+            'fields': ('product_images', 'product_thumbnail')
+        }),
+    )
+
+admin.site.register(InHouseProducts, InHouseProductsAdmin)
--- ecommerce/apis/models.py
+++ ecommerce/apis/models.py
@@ -1,3 +1,77 @@
 from django.db import models
-
+from mptt.models import MPTTModel, TreeForeignKey
 # Create your models here.
+
+class Category(MPTTModel):
+    name = models.CharField(max_length=255)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    image = models.FileField(upload_to="category_img/", null=True, blank=True)
+
+    class MPTTMeta:
+        order_insertion_by = ['name']
+
+    def __str__(self):
+        return self.name
+    
+class Attribute(models.Model):
+    name = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
+class Brand(models.Model):
+    name = models.CharField(max_length=200)        
+    image = models.FileField(upload_to="brand_img/", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+product_type = (
+        ('digital', 'Digital'),
+        ('physical', 'Physical'),
+    )
+    
+discount_type = (
+        ('flat', 'Flat'),
+        ('percent', 'Percent'),
+    )
+    
+products_unit = (
+        ('psc', 'psc'),
+        ('kg', 'kg'),
+        ('ltrs', 'ltrs'),
+        ('gms', 'gms'),
+    )
+
+    
+class InHouseProducts(models.Model):
+    name = models.CharField(max_length=200)    
+    description = models.TextField()
+    product_type = models.CharField(max_length=200, choices=product_type)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sku = models.PositiveIntegerField()
+    unit = models.CharField(max_length=100, choices=products_unit)
+    color = models.CharField(max_length=150)
+    unit_price = models.FloatField()
+    purchase_price = models.FloatField()
+    tax = models.FloatField(null=True, blank=True)
+    discount = models.FloatField(null=True, blank=True)
+    discount_type = models.CharField(max_length=200, choices=discount_type, null=True, blank=True)
+    total_quantity = models.PositiveIntegerField()
+    minimum_order_quantity = models.PositiveBigIntegerField()
+    shipping_cost = models.FloatField(null=True, blank=True)
+    meta_title = models.TextField()
+    meta_description = models.TextField()
+    meta_image = models.FileField(upload_to="meta_image/", null=True, blank=True)
+    product_images = models.FileField(upload_to="products_img", null=True, blank=True)
+    product_thumbnail = models.FileField(upload_to="product_thumbnail/", null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+    
+
+    def get_price(self):
+        price = self.unit_price - (self.unit_price * (self.discount / 100))
+        return price
+    
